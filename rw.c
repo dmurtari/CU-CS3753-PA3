f316e7f6029e376c4370eb41b3fc5991c6f53ade
Domenic Murtari <domenic.murtari@gmail.com>
2014-03-26 11:13:32 -0600

    Fork a given number of processes

--- rw.c
+++ rw.c
@@ -1,9 +1,10 @@
 /*
  * File: rw.c
- * Author: Andy Sayler
+ * Author: Andy Saylor
+ * Modified by: Domenic Murtari
  * Project: CSCI 3753 Programming Assignment 3
  * Create Date: 2012/03/19
- * Modify Date: 2012/03/20
+ * Modify Date: 2014/03/26
  * Description: A small i/o bound program to copy N bytes from an input
  *              file to an output file. May read the input file multiple
  *              times if N is larger than the size of the input file.
@@ -21,6 +22,7 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/wait.h>
 #include <sched.h>
 
 /* Local Defines */
@@ -29,11 +31,14 @@
 #define DEFAULT_OUTPUTFILENAMEBASE "rwoutput"
 #define DEFAULT_BLOCKSIZE 1024
 #define DEFAULT_TRANSFERSIZE 1024*100
+#define DEFAULT_FORKS 10
 
 int main(int argc, char* argv[]){
-
+  
+  int i;
   int rv;
   int policy;
+  int numProcesses;
   int inputFD;
   int outputFD;
   char inputFilename[MAXFILENAMELENGTH];
@@ -51,7 +56,7 @@ int main(int argc, char* argv[]){
   ssize_t bytesWritten = 0;
   ssize_t totalBytesWritten = 0;
   int totalWrites = 0;
-  int inputFileResets = 0
+  int inputFileResets = 0;
   
   /* Process program arguments to select run-time parameters */
   /* Set supplied transfer size or default if not supplied */
@@ -96,6 +101,14 @@ int main(int argc, char* argv[]){
       fprintf(stderr, "Un-handled scheduling policy\n");
       exit(EXIT_FAILURE);
     }
+  }
+  
+  /* Set number of forks if supplied */
+  if(argc < 5){
+    numprocesses = DEFAULT_FORKS;
+  }
+  else{
+    atol(argv[4]);
   }
 
   /* Set supplied input filename or default if not supplied */
@@ -152,101 +165,115 @@ int main(int argc, char* argv[]){
   }
   fprintf(stdout, "New Scheduling Policy: %d\n", sched_getscheduler(0));
 
-  /* Allocate buffer space */
-  buffersize = blocksize;
-  if(!(transferBuffer = malloc(buffersize*sizeof(*transferBuffer)))){
-    perror("Failed to allocate transfer buffer");
-    exit(EXIT_FAILURE);
-  }
+  for(i = 0; i < numProcesses; i++){
+    pid = fork();
+    if(pid == 0){
+      /* Allocate buffer space */
+      buffersize = blocksize;
+      if(!(transferBuffer = malloc(buffersize*sizeof(*transferBuffer)))){
+        perror("Failed to allocate transfer buffer");
+        exit(EXIT_FAILURE);
+      }
 
-  /* Open Input File Descriptor in Read Only mode */
-  if((inputFD = open(inputFilename, O_RDONLY | O_SYNC)) < 0){
-    perror("Failed to open input file");
-    exit(EXIT_FAILURE);
-  }
+      /* Open Input File Descriptor in Read Only mode */
+      if((inputFD = open(inputFilename, O_RDONLY | O_SYNC)) < 0){
+        perror("Failed to open input file");
+        exit(EXIT_FAILURE);
+      }
 
-  /* Open Output File Descriptor in Write Only mode with standard permissions*/
-  rv = snprintf(outputFilename, MAXFILENAMELENGTH, "%s-%d",
-    outputFilenameBase, getpid());    
-  if(rv > MAXFILENAMELENGTH){
-    fprintf(stderr, "Output filename length exceeds limit of %d characters.\n",
-    MAXFILENAMELENGTH);
-    exit(EXIT_FAILURE);
-  }
-  else if(rv < 0){
-    perror("Failed to generate output filename");
-    exit(EXIT_FAILURE);
-  }
-  if((outputFD = open(outputFilename, O_WRONLY | O_CREAT | O_TRUNC | O_SYNC,
-     S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)) < 0){
-    perror("Failed to open output file");
-    exit(EXIT_FAILURE);
-  }
+      /* Open Output File Descriptor in Write Only mode with standard permissions*/
+      rv = snprintf(outputFilename, MAXFILENAMELENGTH, "%s-%d",
+        outputFilenameBase, getpid());    
+      if(rv > MAXFILENAMELENGTH){
+        fprintf(stderr, "Output filename length exceeds limit of %d characters.\n",
+        MAXFILENAMELENGTH);
+        exit(EXIT_FAILURE);
+      }
+      else if(rv < 0){
+        perror("Failed to generate output filename");
+        exit(EXIT_FAILURE);
+      }
+      if((outputFD = open(outputFilename, O_WRONLY | O_CREAT | O_TRUNC | O_SYNC,
+         S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)) < 0){
+        perror("Failed to open output file");
+        exit(EXIT_FAILURE);
+      }
 
-  /* Print Status */
-  fprintf(stdout, "Reading from %s and writing to %s\n",
-    inputFilename, outputFilename);
+      /* Print Status */
+      fprintf(stdout, "Reading from %s and writing to %s\n",
+        inputFilename, outputFilename);
 
-  /* Read from input file and write to output file*/
-  do{
-    /* Read transfersize bytes from input file*/
-    bytesRead = read(inputFD, transferBuffer, buffersize);
-    if(bytesRead < 0){
-      perror("Error reading input file");
+      /* Read from input file and write to output file*/
+      do{
+        /* Read transfersize bytes from input file*/
+        bytesRead = read(inputFD, transferBuffer, buffersize);
+        if(bytesRead < 0){
+          perror("Error reading input file");
+          exit(EXIT_FAILURE);
+        }
+        else{
+          totalBytesRead += bytesRead;
+          totalReads++;
+        }
+
+        /* If all bytes were read, write to output file*/
+        if(bytesRead == blocksize){
+          bytesWritten = write(outputFD, transferBuffer, bytesRead);
+          if(bytesWritten < 0){
+            perror("Error writing output file");
+            exit(EXIT_FAILURE);
+          }
+          else{
+            totalBytesWritten += bytesWritten;
+            totalWrites++;
+          }
+        }
+        /* Otherwise assume we have reached the end of the input file and reset */
+        else{
+          if(lseek(inputFD, 0, SEEK_SET)){
+            perror("Error resetting to beginning of file");
+            exit(EXIT_FAILURE);
+          }
+          inputFileResets++;
+        }
+
+      }while(totalBytesWritten < transfersize);
+
+      /* Output some possibly helpfull info to make it seem like we were doing stuff */
+      fprintf(stdout, "Read:    %zd bytes in %d reads\n",
+        totalBytesRead, totalReads);
+      fprintf(stdout, "Written: %zd bytes in %d writes\n",
+        totalBytesWritten, totalWrites);
+      fprintf(stdout, "Read input file in %d pass%s\n",
+        (inputFileResets + 1), (inputFileResets ? "es" : ""));
+      fprintf(stdout, "Processed %zd bytes in blocks of %zd bytes\n",
+        transfersize, blocksize);
+
+      /* Free Buffer */
+      free(transferBuffer);
+
+      /* Close Output File Descriptor */
+      if(close(outputFD)){
+        perror("Failed to close output file");
+        exit(EXIT_FAILURE);
+      }
+
+      /* Close Input File Descriptor */
+      if(close(inputFD)){
+        perror("Failed to close input file");
+        exit(EXIT_FAILURE);
+      }
+    } else if(pid > 0) {
+      printf("Forked child %d", pid);
+    } else {
+      fprintf(stderr, "Forking failed");
       exit(EXIT_FAILURE);
     }
-    else{
-      totalBytesRead += bytesRead;
-      totalReads++;
-    }
-
-    /* If all bytes were read, write to output file*/
-    if(bytesRead == blocksize){
-      bytesWritten = write(outputFD, transferBuffer, bytesRead);
-      if(bytesWritten < 0){
-        perror("Error writing output file");
-        exit(EXIT_FAILURE);
-      }
-      else{
-        totalBytesWritten += bytesWritten;
-        totalWrites++;
-      }
-    }
-    /* Otherwise assume we have reached the end of the input file and reset */
-    else{
-      if(lseek(inputFD, 0, SEEK_SET)){
-        perror("Error resetting to beginning of file");
-        exit(EXIT_FAILURE);
-      }
-      inputFileResets++;
-    }
-
-  }while(totalBytesWritten < transfersize);
-
-  /* Output some possibly helpfull info to make it seem like we were doing stuff */
-  fprintf(stdout, "Read:    %zd bytes in %d reads\n",
-    totalBytesRead, totalReads);
-  fprintf(stdout, "Written: %zd bytes in %d writes\n",
-    totalBytesWritten, totalWrites);
-  fprintf(stdout, "Read input file in %d pass%s\n",
-    (inputFileResets + 1), (inputFileResets ? "es" : ""));
-  fprintf(stdout, "Processed %zd bytes in blocks of %zd bytes\n",
-    transfersize, blocksize);
-
-  /* Free Buffer */
-  free(transferBuffer);
-
-  /* Close Output File Descriptor */
-  if(close(outputFD)){
-    perror("Failed to close output file");
-    exit(EXIT_FAILURE);
   }
 
-  /* Close Input File Descriptor */
-  if(close(inputFD)){
-    perror("Failed to close input file");
-    exit(EXIT_FAILURE);
+  for(i = 0; i < numProcesses; i++){
+    wait(NULL);  
   }
-
+  
   return EXIT_SUCCESS;
 }
