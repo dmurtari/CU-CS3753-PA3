1e7b200b96fa370a49ed24f9ecb981d84e244432
Domenic Murtari <domenic.murtari@gmail.com>
2014-03-28 09:01:32 -0600

    Header info, renamed

--- rw-sched.c
+++ rw-sched.c
@@ -4,7 +4,7 @@
  * Modified by: Domenic Murtari
  * Project: CSCI 3753 Programming Assignment 3
  * Create Date: 2012/03/19
- * Modify Date: 2014/03/26
+ * Modify Date: 2014/03/28
  * Description: A small i/o bound program to copy N bytes from an input
  *              file to an output file. May read the input file multiple
  *              times if N is larger than the size of the input file.
